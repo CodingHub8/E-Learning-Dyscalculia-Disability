938cf151209b479e2d107a2233869877166007b4
ManBil <manbil6146@gmail.com>
2025-02-28 03:17:38 +0800

    Initial commit

---
+++ src/main/java/com/example/eLearningDyscalculiaDisability/ELearningDyscalculiaDisabilityApplication.java
@@ -0,0 +1,13 @@
+package com.example.eLearningDyscalculiaDisability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELearningDyscalculiaDisabilityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ELearningDyscalculiaDisabilityApplication.class, args);
+    }
+
+}
