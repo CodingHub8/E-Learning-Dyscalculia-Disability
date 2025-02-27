f90d211997ab4c8351944779dfbcd796681154f5
ManBil <manbil6146@gmail.com>
2025-02-28 02:05:05 +0800

    Initial Commit

---
+++ E-Learning-Dyscalculia-Disability/src/main/java/com/example/eLearningDyscalculiaDisability/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.eLearningDyscalculiaDisability.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")  // When user visits "/", return index.html
+    public String home(Model model) {
+        model.addAttribute("message", "Welcome to the E-Learning Platform!");
+        return "index"; // This maps to templates/index.html
+    }
+}
