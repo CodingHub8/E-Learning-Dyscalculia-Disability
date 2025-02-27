6f4b3ab27572b99560a470d5c52da083e4cf8cd1
ManBil <manbil6146@gmail.com>
2025-02-28 02:33:22 +0800

    Initial Commit

--- E-Learning-Dyscalculia-Disability/src/main/java/com/example/eLearningDyscalculiaDisability/controllers/HomeController.java
+++ E-Learning-Dyscalculia-Disability/src/main/java/com/example/eLearningDyscalculiaDisability/controllers/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 
     @GetMapping("/")  // When user visits "/", return index.html
     public String home(Model model) {
-        model.addAttribute("message", "Welcome to the E-Learning Platform!");
+        model.addAttribute("message", "Welcome to the E-Learning Platform, Blyat!");
         return "index"; // This maps to templates/index.html
     }
 }
