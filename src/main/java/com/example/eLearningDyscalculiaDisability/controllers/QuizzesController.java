fbc4d82c6281c2c476cb9f0934c163a5696407d1
myazid350 <myazid070@gmail.com>
2025-03-13 23:18:03 +0800

    updated performance , and dashboard quiz stat with admin login fixed

--- src/main/java/com/example/eLearningDyscalculiaDisability/controllers/QuizzesController.java
+++ src/main/java/com/example/eLearningDyscalculiaDisability/controllers/QuizzesController.java
@@ -1,21 +1,24 @@
 package com.example.eLearningDyscalculiaDisability.controllers;
 
-import com.example.eLearningDyscalculiaDisability.model.Quiz;
-import com.example.eLearningDyscalculiaDisability.model.QuizResult;
-import com.example.eLearningDyscalculiaDisability.repository.QuizRepository;
-import com.example.eLearningDyscalculiaDisability.repository.QuizResultRepository;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import com.example.eLearningDyscalculiaDisability.model.Quiz;
+import com.example.eLearningDyscalculiaDisability.model.QuizResult;
+import com.example.eLearningDyscalculiaDisability.repository.QuizRepository;
+import com.example.eLearningDyscalculiaDisability.repository.QuizResultRepository;
 
 @Controller
 public class QuizzesController {
